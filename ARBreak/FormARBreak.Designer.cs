a984c06e0d34113423f8cbc2a1f5ffb1255d27f8
Abhinav Rathod <abhinav.rathod@fove-inc.com>
2018-05-12 21:36:59 -0400

    Working Code

--- ARBreak/FormARBreak.Designer.cs
+++ ARBreak/FormARBreak.Designer.cs
@@ -31,18 +31,22 @@
             this.btnStart = new System.Windows.Forms.Button();
             this.lblTimeLeft = new System.Windows.Forms.Label();
             this.btnRestart = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.btnStop = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnStart
             // 
+            this.btnStart.BackColor = System.Drawing.Color.Lime;
             this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.ForeColor = System.Drawing.Color.Black;
             this.btnStart.Location = new System.Drawing.Point(72, 159);
             this.btnStart.Name = "btnStart";
             this.btnStart.Size = new System.Drawing.Size(140, 116);
             this.btnStart.TabIndex = 0;
             this.btnStart.Text = "Start";
-            this.btnStart.UseVisualStyleBackColor = true;
-            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.BtnStart_ClickAsync);
             // 
             // lblTimeLeft
             // 
@@ -56,20 +60,46 @@
             // 
             // btnRestart
             // 
+            this.btnRestart.BackColor = System.Drawing.SystemColors.MenuHighlight;
             this.btnRestart.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnRestart.Location = new System.Drawing.Point(72, 358);
             this.btnRestart.Name = "btnRestart";
             this.btnRestart.Size = new System.Drawing.Size(140, 116);
             this.btnRestart.TabIndex = 2;
             this.btnRestart.Text = "Restart";
-            this.btnRestart.UseVisualStyleBackColor = true;
-            this.btnRestart.Click += new System.EventHandler(this.btnRestart_Click);
+            this.btnRestart.UseVisualStyleBackColor = false;
+            this.btnRestart.Click += new System.EventHandler(this.btnRestart_ClickAsync);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.Location = new System.Drawing.Point(96, 311);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(116, 24);
+            this.lblStatus.TabIndex = 3;
+            this.lblStatus.Text = "Not Running";
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackColor = System.Drawing.Color.Red;
+            this.btnStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.Location = new System.Drawing.Point(72, 500);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(140, 116);
+            this.btnStop.TabIndex = 4;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = false;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
             // 
             // FormARBreak
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(291, 554);
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(291, 655);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.lblStatus);
             this.Controls.Add(this.btnRestart);
             this.Controls.Add(this.lblTimeLeft);
             this.Controls.Add(this.btnStart);
@@ -79,6 +109,7 @@
             this.Name = "FormARBreak";
             this.Text = "ARBreak";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.OnQuit_Clicked);
+            this.Load += new System.EventHandler(this.FormARBreak_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -89,6 +120,8 @@
         private System.Windows.Forms.Button btnStart;
         private System.Windows.Forms.Label lblTimeLeft;
         private System.Windows.Forms.Button btnRestart;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button btnStop;
     }
 }
 
--- ARBreak/FormARBreak.cs
+++ ARBreak/FormARBreak.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
+using System.Diagnostics;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,24 +8,103 @@ namespace ARBreak
 {
     public partial class FormARBreak : Form
     {
+        Process batchProcess;
+        bool isProcessRunning = true;
+        string absolutePath = @"C:\Users\User\Desktop\";
         public FormARBreak()
         {
             InitializeComponent();
         }
 
-        private void btnStart_Click(object sender, EventArgs e)
+        public Task WaitForExitAsync(CancellationToken cancellationToken = default(CancellationToken))
         {
+            var tcs = new TaskCompletionSource<object>(TaskCreationOptions.AttachedToParent);
+            batchProcess.EnableRaisingEvents = true;
+            batchProcess.Exited += (sender, args) => tcs.TrySetResult(null);
+            if (cancellationToken != default(CancellationToken))
+                cancellationToken.Register( () => { tcs.TrySetCanceled(); } );
+            return tcs.Task;
+        }
 
+        private void UpdateTimeLeftLabel()
+        {
+            while ( isProcessRunning )
+            {
+                System.IO.StreamReader file = new System.IO.StreamReader(absolutePath + @"ARBreak_.txt");
+                string readLine = "", line;
+                while ((line = file.ReadLine()) != null)
+                {
+                    readLine = line;
+                }
+                file.Close();
+                // To pass back control to Main Thread to update UI
+                // Since UI can only be updated on the main thread
+                this.lblTimeLeft.Invoke( (MethodInvoker) delegate {
+                    this.lblTimeLeft.Text = readLine.Split(' ')[0] + " minutes left";
+                } );
+                Thread.Sleep(1000);
+            }
+        }
+
+        private void StartBatchScript()
+        {
+            batchProcess = new Process();
+            batchProcess.StartInfo.FileName = absolutePath + "ARBreak_Start.bat";
+            batchProcess.StartInfo.UseShellExecute = false;
+            batchProcess.StartInfo.RedirectStandardInput = true;
+            batchProcess.StartInfo.CreateNoWindow = true;
+            batchProcess.Start();
+            isProcessRunning = true;
+            new Task(UpdateTimeLeftLabel).Start();
+        }
+
+        private void CloseBatchScript()
+        {
+            if (batchProcess != null && batchProcess.StartInfo != null)
+            {
+                try
+                {
+                    batchProcess.CloseMainWindow();
+                    foreach (var process in Process.GetProcessesByName("cscript"))
+                    {
+                        process.Kill();
+                    }
+                }
+                catch(InvalidOperationException)
+                {
+
+                }
+            }
+            isProcessRunning = false;
+            lblTimeLeft.Text = "-- minutes left";
+        }
+
+        private async void BtnStart_ClickAsync(object sender, EventArgs e)
+        {
+            StartBatchScript();
+            await WaitForExitAsync();
         }
 
         private void OnQuit_Clicked(object sender, FormClosingEventArgs e)
         {
+            CloseBatchScript();
+        }
+
+        private async void btnRestart_ClickAsync(object sender, EventArgs e)
+        {
+            CloseBatchScript();
+            StartBatchScript();
+            await WaitForExitAsync();
+        }
+
+        private void FormARBreak_Load(object sender, EventArgs e)
+        {
 
         }
 
-        private void btnRestart_Click(object sender, EventArgs e)
+        private void btnStop_Click(object sender, EventArgs e)
         {
-
+            CloseBatchScript();
         }
     }
 }
