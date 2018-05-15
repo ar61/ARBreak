2489c8f05da04fe948275896eac71acd2390bed1
Abhinav <vectorhashmap@gmail.com>
2018-05-15 15:58:27 -0400

    Updated processid based data
    - Fixed up paths
    - Cleanup of data file
    - Disable Start button once process is running

--- ARBreak/FormARBreak.cs
+++ ARBreak/FormARBreak.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Diagnostics;
 using System.Threading;
 using System.Threading.Tasks;
@@ -8,103 +8,138 @@ namespace ARBreak
 {
     public partial class FormARBreak : Form
     {
-        Process batchProcess;
+        Process vbProcess;
         bool isProcessRunning = true;
-        string absolutePath = @"C:\Users\User\Desktop\";
+        string tmpPath = @"";
+        string absolutePath;
+        int processId = 0;
+        
         public FormARBreak()
         {
             InitializeComponent();
         }
 
-        public Task WaitForExitAsync(CancellationToken cancellationToken = default(CancellationToken))
+        private void FormARBreak_Load(object sender, EventArgs e)
         {
-            var tcs = new TaskCompletionSource<object>(TaskCreationOptions.AttachedToParent);
-            batchProcess.EnableRaisingEvents = true;
-            batchProcess.Exited += (sender, args) => tcs.TrySetResult(null);
-            if (cancellationToken != default(CancellationToken))
-                cancellationToken.Register( () => { tcs.TrySetCanceled(); } );
+            setTempFolderPath();
+#if DEBUG
+            absolutePath = @"C:\Users\arathod16\Desktop\ARBreak-master\ARBreak-master\Helper_Files\";
+#else
+            absolutePath = @"";
+#endif
+        }
+
+        public void setTempFolderPath()
+        {
+            tmpPath = System.Environment.GetEnvironmentVariable("tmp");
+        }
+
+        public Task<int> WaitForExitAsync()//CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var tcs = new TaskCompletionSource<int>(TaskCreationOptions.AttachedToParent);
+            vbProcess.EnableRaisingEvents = true;
+            vbProcess.Exited += (sender, args) => tcs.TrySetResult(0);
+            //if (cancellationToken != default(CancellationToken))
+            //    cancellationToken.Register( () => { tcs.TrySetCanceled(); } );
             return tcs.Task;
         }
 
         private void UpdateTimeLeftLabel()
         {
+            // Wait for 5 seconds for the vb script to write the correct value to file
+            Thread.Sleep(1000 * 5);
+            processId = vbProcess.Id;
             while ( isProcessRunning )
-            {
-                System.IO.StreamReader file = new System.IO.StreamReader(absolutePath + @"ARBreak_.txt");
-                string readLine = "", line;
-                while ((line = file.ReadLine()) != null)
-                {
-                    readLine = line;
-                }
-                file.Close();
-                // To pass back control to Main Thread to update UI
-                // Since UI can only be updated on the main thread
-                this.lblTimeLeft.Invoke( (MethodInvoker) delegate {
-                    this.lblTimeLeft.Text = readLine.Split(' ')[0] + " minutes left";
-                } );
-                Thread.Sleep(1000);
-            }
-        }
-
-        private void StartBatchScript()
-        {
-            batchProcess = new Process();
-            batchProcess.StartInfo.FileName = absolutePath + "ARBreak_Start.bat";
-            batchProcess.StartInfo.UseShellExecute = false;
-            batchProcess.StartInfo.RedirectStandardInput = true;
-            batchProcess.StartInfo.CreateNoWindow = true;
-            batchProcess.Start();
-            isProcessRunning = true;
-            new Task(UpdateTimeLeftLabel).Start();
-        }
-
-        private void CloseBatchScript()
-        {
-            if (batchProcess != null && batchProcess.StartInfo != null)
             {
                 try
                 {
-                    batchProcess.CloseMainWindow();
-                    foreach (var process in Process.GetProcessesByName("cscript"))
+                    System.IO.StreamReader file = new System.IO.StreamReader(tmpPath + @"\ARBreak_data_" + processId + ".txt");
+                    string readLine = "", line;
+                    while ((line = file.ReadLine()) != null)
                     {
-                        process.Kill();
+                        readLine = line;
                     }
+                    file.Close();
+                    // To pass back control to Main Thread to update UI
+                    // Since UI can only be updated on the main thread
+                    this.lblTimeLeft.Invoke((MethodInvoker)delegate {
+                        this.lblTimeLeft.Text = readLine.Split(' ')[0] + " minutes left";
+                    });
+                }
+                catch(System.IO.FileNotFoundException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                Thread.Sleep(1000 * 30);
+            }
+        }
+
+        private void StartVbScript()
+        {
+            vbProcess = new Process();
+            vbProcess.StartInfo.FileName = @"C:\WINDOWS\SysWOW64\cscript.exe";
+            vbProcess.StartInfo.Arguments = absolutePath + @"ARBreak.vbs";
+            vbProcess.StartInfo.UseShellExecute = false;
+            //vbProcess.StartInfo.RedirectStandardInput = true;
+            vbProcess.StartInfo.CreateNoWindow = true;
+            vbProcess.Start();
+            isProcessRunning = true;
+            // create a new file in %tmp% unique to this instance
+            new Task(UpdateTimeLeftLabel).Start();
+        }
+
+        private void CleanupFile()
+        {
+            // Delete temp log file if vb script is killed by force
+            if (System.IO.File.Exists(tmpPath + @"\ARBreak_data_" + processId + ".txt"))
+            {
+                System.IO.File.Delete(tmpPath + @"\ARBreak_data_" + processId + ".txt");
+            }
+        }
+
+        private void CloseVbScript()
+        {
+            if (vbProcess != null && vbProcess.StartInfo != null)
+            {
+                try
+                {
+                    //vbProcess.CloseMainWindow();
+                    vbProcess.Kill();
                 }
                 catch(InvalidOperationException)
                 {
-
+                    // Ignore as process may not exist on multiple 
                 }
             }
             isProcessRunning = false;
             lblTimeLeft.Text = "-- minutes left";
+            CleanupFile();
         }
 
         private async void BtnStart_ClickAsync(object sender, EventArgs e)
         {
-            StartBatchScript();
+            btnStart.Enabled = false;
+            StartVbScript();
             await WaitForExitAsync();
         }
 
         private void OnQuit_Clicked(object sender, FormClosingEventArgs e)
         {
-            CloseBatchScript();
+            CloseVbScript();
         }
 
         private async void btnRestart_ClickAsync(object sender, EventArgs e)
         {
-            CloseBatchScript();
-            StartBatchScript();
+            btnStart.Enabled = true;
+            CloseVbScript();
+            StartVbScript();
             await WaitForExitAsync();
-        }
-
-        private void FormARBreak_Load(object sender, EventArgs e)
-        {
-
         }
 
         private void btnStop_Click(object sender, EventArgs e)
         {
-            CloseBatchScript();
+            btnStart.Enabled = true;
+            CloseVbScript();
         }
     }
 }
